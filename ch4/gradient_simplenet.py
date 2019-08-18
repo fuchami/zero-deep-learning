916576d90fb25349e63bacde2006a3937a00047d
fuchami <famichiki.yuuki@gmail.com>
2019-08-19 02:35:05 +0900

    finished chapter4

---
+++ ch4/gradient_simplenet.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+
+import sys,os
+# 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir) 
+
+import numpy as np
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+    
+    def predict(self,x):
+        return np.dot(x, self.W)
+    
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+net = simpleNet()
+print(net.W) # 重みパラデータ
+
+x = np.array([0.6, 0.9])
+p = net.predict(x)
+print(p)
+print(np.argmax(p))
+
+t = np.array([0,0,1]) # 正解ラベル
+print(net.loss(x,t))
+
+def f(W):
+    return net.loss(x, t)
+
+# f = lambda w: net.loss(x, t) これでも可
+
+dW = numerical_gradient(f, net.W)
+print(dW)
--- common/functions.py
+++ common/functions.py
@@ -5,9 +5,25 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+
 def softmax(a):
     c = np.max(a)
     exp_a = np.exp(a - c) # オーバーフロー対策
     sum_exp_a = np.sum(exp_a)
     
     return exp_a / sum_exp_a
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    # 教師データがone-hotの場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
